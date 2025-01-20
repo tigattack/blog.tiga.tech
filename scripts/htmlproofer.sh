02fa938a154869de4ca1fa28c4905ce91d1e1420
tigattack <10629864+tigattack@users.noreply.github.com>
2025-01-20 03:33:47 +0000

    chore: update gitignore, htmlproofer script

--- scripts/htmlproofer.sh
+++ scripts/htmlproofer.sh
@@ -5,12 +5,18 @@ build_dir=$(mktemp -d)
 
 hugo build --gc -d "${build_dir}"
 
-default_args=(--typhoeus="{\"headers\":{\"User-Agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0\"}}" --cache='{"timeframe": {"external": "30d"}, "storage_dir": "/cache/.htmlproofer/"}')
+default_args=(
+  --ignore_missing_alt
+  --ignore_status_codes 403
+  --checks Links,Images,Scripts,Favicon,OpenGraph
+  --typhoeus="{\"headers\":{\"User-Agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0\"}}"
+  --cache='{"timeframe": {"external": "30d"}, "storage_dir": "/cache/.htmlproofer/"}'
+)
 
-docker run --rm -it \
+docker run --rm \
   -v "${build_dir}:/src:ro" \
   -v "${project_dir}/tmp:/cache:rw" \
-  parkr/html-proofer:latest \
+  parkr/html-proofer:5.0.9 \
   "${default_args[@]}" "$@"
 
 rm -rf "${build_dir}"
