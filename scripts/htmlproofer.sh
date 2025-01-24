732045e02dbaa7e4c9b30857eceeb3cdf8a347f7
tigattack <10629864+tigattack@users.noreply.github.com>
2025-01-24 12:36:35 +0000

    scripts/htmlproofer: add args

--- scripts/htmlproofer.sh
+++ scripts/htmlproofer.sh
@@ -6,11 +6,13 @@ build_dir=$(mktemp -d)
 hugo build --gc -d "${build_dir}"
 
 default_args=(
+  /src
   --ignore_missing_alt
   --ignore_status_codes 403
   --checks Links,Images,Scripts,Favicon,OpenGraph
   --typhoeus="{\"headers\":{\"User-Agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0\"}}"
   --cache='{"timeframe": {"external": "30d"}, "storage_dir": "/cache/.htmlproofer/"}'
+  --swap_urls 'https\:\/\/blog.tiga.tech/:/'
 )
 
 docker run --rm \
